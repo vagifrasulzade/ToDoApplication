f3a5c051c36635b793d642eaedd978885598ddcc
Rasul_zq69 <Rasul_zq69@STHQ0116-04>
2025-09-17 11:00:39 +0400

    update 3

--- ToDoApp/obj/Debug/net8.0/ToDoApp.AssemblyInfo.cs
+++ ToDoApp/obj/Debug/net8.0/ToDoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a50222e76f9aa5a00bb6af9014310275079a91ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
